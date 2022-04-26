e1296f07340c51d4db3c63e5a24ea5eb0d88a794
byaskew3 <byaskew3@gmail.com>
2022-04-26 14:04:14 -0600

    JS: no-data-message

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
-const searchBtn = document.querySelector('.btn')
+const searchBtn = document.querySelector('.btn-search')
+const watchListBtn = document.querySelector('.btn-watchlist')
 const inputEl = document.querySelector('input')
 const mainContainerEl = document.querySelector('.main-container')
 const filmContainerEl = document.querySelector('.film-container')
+let addedToWatchlist = false;
+
+const addToWatchlist = () => {
+    if (clickHandler) {
+
+    }
+}
+
+const clickHandler = () => addedToWatchlist = true;
 
 searchBtn.addEventListener('click', () => {
     mainContainerEl.innerHTML = ''
     fetch(`http://www.omdbapi.com/?s=${inputEl.value}&apikey=855731ad`)
         .then(res => res.json())
         .then(data => {
+            let results = []
             inputEl.value = ''
-            filmContainerEl.style.display = 'none'
-            for (let movie of data.Search) {
-                fetch(`http://www.omdbapi.com/?t=${movie.Title}&apikey=855731ad`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    mainContainerEl.innerHTML += `
-                    <div class="movie">
-                        <img src="${data.Poster}" alt="">
-                        <div class="movie-text">
-                            <h4>${data.Title} <span class="star-text"><i class="fa-solid fa-star star"></i> ${data.imdbRating}</span></h4>
-                            <p>${data.Runtime} <span class="movie-types">${data.Genre}</span><span class="watchlist-text"><i class="fa-solid fa-circle-plus watchlist"></i>Watchlist</span></p>
-                            <p class="movie-desc">${data.Plot}</p>
+            console.log(data.Response)
+            if (data.Response === 'True') {
+                filmContainerEl.style.display = 'none'
+                for (let movie of data.Search) {
+                    fetch(`http://www.omdbapi.com/?t=${movie.Title}&apikey=855731ad`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        results.push(data)
+                        mainContainerEl.innerHTML += `
+                        <div class="movie">
+                            <img src="${data.Poster}" alt="">
+                            <div class="movie-text">
+                                <h4>${data.Title} <span class="star-text"><i class="fa-solid fa-star star"></i> ${data.imdbRating}</span></h4>
+                                <p>${data.Runtime} <span class="movie-types">${data.Genre}</span>
+                                <span class="watchlist-text"><button class="btn-watchlist"><i class="fa-solid fa-circle-plus watchlist"></i>Watchlist</button></span>
+                                </p>
+                                <p class="movie-desc">${data.Plot}</p>
+                            </div>
                         </div>
-                    </div>
-                    `
-                })
+                        `
+                        // console.log(results)
+                        // watchListBtn.addEventListener('click', () => {
+                        //     if 
+                        // })
+                    })
+            }
+            } else {
+                filmContainerEl.innerHTML = `
+                <p class="no-data-text">Unable to find what you're looking for. Please try another search.</p>
+                `
+                filmContainerEl.style.display = 'flex'
             }
         })
 })
